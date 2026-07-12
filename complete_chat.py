63c534bfc50d5408ad3d51c47bb918e7ef9a892a
mvbantas74 <mvbantas74@gmail.com>
2026-07-12 22:04:40 +0300

    Refactor Gemini class and update model name

--- complete_chat.py
+++ complete_chat.py
@@ -23,7 +23,7 @@ class Gemini:
         for item in generator:
             yield item.text
 
-chat_instance = Gemini("gemini-3-flash")
+chat_instance = Gemini("gemini-flash-latest")
 
 if 'messages' not in st.session_state:
     st.session_state.messages = []
