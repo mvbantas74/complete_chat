2a3aa9bbfa79b5ec06f9e59fa89ae34fc98a1944
mvbantas74 <mvbantas74@gmail.com>
2026-07-26 08:18:34 +0300

    Update complete_chat.py

--- complete_chat.py
+++ complete_chat.py
@@ -58,15 +58,6 @@ class Gemini:
                 )
             )
         )
-        
-        """gemini_format = ""
-        for item in content:
-            gemini_format += f"`{item['role']}`: {item['content']}"
-            gemini_format += "\n"
-        return self.client.models.generate_content_stream(
-            model=self.model,
-            contents=gemini_format,
-        )"""
 
     @staticmethod
     def parse_generator(generator):
