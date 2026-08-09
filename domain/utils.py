209e3ab88731a31e9b45c99ce2313e7f786ef014
mvbantas74 <mvbantas74@gmail.com>
2026-08-09 18:46:34 +0300

    Create utils.py

---
+++ domain/utils.py
@@ -0,0 +1,17 @@
+import base64
+from Pillow import Image
+
+def reduce_image_size(file_input: UploadedFile) -> bytes:
+    img = Image.open(file_input)
+    if img.mode in ("RGBA", "P"):
+        img = img.convert("RGB")
+    img.thumbnail((1024, 1024))
+    buffer = io.BytesIO()
+    img.save(buffer, format="JPEG", quality=75)
+    return buffer.getvalue()
+
+def img_from_bytes_to_b64(img_bytes: bytes) -> str:
+    return base64.b64encode(img_bytes).decode()
+
+def img_from_b64_to_bytes(img_input: str) -> bytes:
+    return base64.b64decode(img_input.encode())
